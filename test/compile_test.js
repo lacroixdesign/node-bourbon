c9f4072032fea6666529dd5543881c647a3d690e
Michael LaCroix <michael@lacroixdesign.net>
2014-01-25 16:34:19 -0600

    test against the examples used in Bourbon's docs

--- test/compile_test.js
+++ test/compile_test.js
@@ -4,14 +4,29 @@ var expect  = require('chai').expect
   , fs      = require('fs');
 
 describe('node-bourbon', function() {
+
   it('should compile to css when importing Bourbon', function() {
     var generatedCss = sass.renderSync({
-      file: __dirname + '/fixtures/source.scss',
+      file: __dirname + '/fixtures/compile.scss',
       includePaths: bourbon,
       outputStyle: 'expanded'
     });
-    var expectedCssFile = __dirname + '/expectations/compiled.css';
+    var expectedCssFile = __dirname + '/expectations/compile.css';
     var expectedCss     = fs.readFileSync(expectedCssFile, {encoding: 'utf8'});
     expect(generatedCss).to.eq(expectedCss);
   });
+
+  it('should not throw errors for Bourbon features', function(done) {
+    sass.render({
+      file: __dirname + '/fixtures/features.scss',
+      includePaths: bourbon,
+      error: function(err) {
+        throw new Error(err);
+      },
+      success: function(css) {
+        done();
+      }
+    });
+  });
+
 });
