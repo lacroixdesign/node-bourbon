07e3ab1955aab082b7d6ce2b95194bcefd775aab
Michael LaCroix <michael@lacroixdesign.net>
2016-03-21 14:05:59 -0500

    Don't return a nested array of paths

--- index.js
+++ index.js
@@ -2,12 +2,10 @@ var path = require('path');
 var bourbon = require('bourbon');
 
 module.exports = {
-
-  includePaths: [bourbon.includePaths],
+  includePaths: bourbon.includePaths,
 
   with: function() {
     var paths  = Array.prototype.slice.call(arguments);
     return [].concat.apply([bourbon.includePaths], paths);
   }
-
 };
